b3904306b4ba04700894cdd1702645fcc4f7d6cd
Sri Aradhyula <sraradhy@cisco.com>
2025-06-04 15:53:39 -0500

    fix: use buildx and multi-arch images
    Signed-off-by: Sri Aradhyula <sraradhy@cisco.com>

--- push.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-docker image tag cnoe-io/jarvis-docker-env-arm64:latest haskalpa/jarvis-docker-env:arm64-latest
-docker push haskalpa/jarvis-docker-env:arm64-latest
-docker image tag cnoe-io/jarvis-docker-env:amd64-latest haskalpa/jarvis-docker-env:amd64-latest
-docker push haskalpa/jarvis-docker-env:amd64-latest
